b7d2645d33d635c1db470129dcd3da8b5a8b6e22
neha <nehasinghal@Nehas-MacBook-Air.local>
2025-04-18 21:36:47 +0200

    creat user route

---
+++ app/models/user.py
@@ -0,0 +1,34 @@
+from sqlmodel import SQLModel, Field
+from typing import Optional
+from enum import Enum
+from datetime import datetime
+
+class UserType(str, Enum):
+    user = "user"
+    ngo = "ngo"
+
+class UserBase(SQLModel):
+    name: str = Field(index=True)
+    email: Optional[str] = Field(default=None, index=True, unique=True)
+    location: str
+    user_type: UserType
+
+class User(UserBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    password: str
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    updated_at: datetime = Field(default_factory=datetime.utcnow)
+
+
+class UserCreate(UserBase):
+    password: str
+
+class UserPublic(UserBase):
+    id : int
+
+class UserUpdate(UserBase):
+    name: str | None = None
+
+
+
+
